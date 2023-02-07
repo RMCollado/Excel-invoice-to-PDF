89aecae5ed1d862eb5f958c4e1d75bdb6deb34d3
Rich <Richardmcollado@gmail.com>
2023-02-07 18:03:42 -0500

    Added invoice date

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pandas as pd
 import glob
 from fpdf import FPDF
+from fpdf.enums import XPos, YPos
 from pathlib import Path
 
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
+    invoice_nr, date = filename.split("-")
+
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice number: {invoice_nr}")
+    pdf.cell(w=50, h=8, txt=f"Invoice number: {invoice_nr}", new_x=XPos.LMARGIN, new_y=YPos.NEXT)
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}")
+
     pdf.output(f"PDFs/{filename}.pdf")
